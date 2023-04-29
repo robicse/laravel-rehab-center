bc1c8735b204eccce10d89900ad35d2bd780a75a
Zahidul <mjahid1990@gmail.com>
2023-04-29 18:34:29 +0600

    code update

--- resources/views/frontend/welcome.blade.php
+++ resources/views/frontend/welcome.blade.php
@@ -466,31 +466,7 @@ viewBox="0 0 50 50">
 @push('js')
 <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 <script>
-    $( function() {
-      var availableTags = [
-        "ActionScript",
-        "AppleScript",
-        "Asp",
-        "BASIC",
-        "C",
-        "C++",
-        "Clojure",
-        "COBOL",
-        "ColdFusion",
-        "Erlang",
-        "Fortran",
-        "Groovy",
-        "Haskell",
-        "Java",
-        "JavaScript",
-        "Lisp",
-        "Perl",
-        "PHP",
-        "Python",
-        "Ruby",
-        "Scala",
-        "Scheme"
-      ];
+
       $( "#tags" ).autocomplete({
         source: function (request, response) {
         $.ajax({
@@ -508,6 +484,6 @@ viewBox="0 0 50 50">
     },
         
       });
-    } );
+
     </script>
 @endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
      <!-- CSRF Token -->
      <meta name="csrf-token" content="{{ csrf_token() }}">
+     <!--============================= Developer: zahidul1994@github.com =============================-->
+     <!--============================= Email: mjahid1990@gmail.com =============================-->
      {!! SEO::generate() !!}
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">
